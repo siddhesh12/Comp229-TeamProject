52373c7dca6663358ea244cefb98d168f57b1b59
Siddhesh <Siddhesh@SIDDHESH-PC>
2016-11-16 13:57:32 -0800

    Add project files.

---
+++ PersonalLibraryProject_comp229/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace PersonalLibraryProject_comp229.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ PersonalLibraryProject_comp229/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalLibraryProject_comp229.Global" Language="C#" %>
