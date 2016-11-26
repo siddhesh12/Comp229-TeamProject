2832fd38dc449c4a70e29f503cc71fee35a166c0
Aakash <achristian3110@gmail.com>
2016-11-25 19:49:20 -0800

    Detail page SQL query Implemented

--- PersonalLibraryProject_comp229/Pages/detailView.aspx.cs
+++ PersonalLibraryProject_comp229/Pages/detailView.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,45 @@ namespace PersonalLibraryProject_comp229.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                bindList();
+            }
+        }
 
+        protected void bindList()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnectionString"].ToString());
+            SqlCommand comm = new SqlCommand("SELECT b.book_name, b.book_detail, b.date FROM Comp229TeamProject.dbo.Books b WHERE b.book_id = 16", connection);
+            SqlCommand commReview = new SqlCommand("SELECT r.reviews_msg, u.name FROM Comp229TeamProject.dbo.Reviews r JOIN Comp229TeamProject.dbo.Users u ON(r.user_id = u.user_id) WHERE r.book_id = 16", connection);
+
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                detailGridView.DataSource = reader;
+                detailGridView.DataBind();
+                reader.Close();
+
+                SqlDataReader readerReview = commReview.ExecuteReader();
+                reviewGrid.DataSource = readerReview;
+                reviewGrid.DataBind();
+                readerReview.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        protected void detailGridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void submitButtonClicked(object sender, EventArgs e)
+        {
+            Response.Redirect("MainTrackingPage.aspx");
         }
     }
 }
